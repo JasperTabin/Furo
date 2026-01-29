59ecb71dec1700ee337579920703a59f85700ced
Jasper Tabin <tabinjasper@gmail.com>
2026-01-30 00:46:15 +0800

    Structured the project folder.

---
+++ src/utils/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-2xl font-semibold">
+              Furē - Means flow in Japanese
+      </p>
+    </div>
+  );
+}
